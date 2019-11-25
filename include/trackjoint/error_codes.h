2fee8fe321f5a3bf9cd54c842a90e0793a07d92e
AndyZe <andyzelenak@verbsurgical.com>
2019-11-25 16:49:20 -0600

    Remove irrelevant error codes

--- include/trackjoint/error_codes.h
+++ include/trackjoint/error_codes.h
@@ -15,27 +15,19 @@ namespace trackjoint {
 
 enum ErrorCodeEnum {
   kNoError = 0,
-  kQuaternionLengthMismatch = 1,
-  kTranslationRotationLengthMismatch = 2,
-  kDesiredDurationTooShort = 3,
-  kCurrentOrientationNotNormalized = 4,
-  kGoalOrientationNotNormalized = 5,
-  kMaxDurationExceeded = 6,
-  kVelocityExceedsLimit = 7,
-  kAccelExceedsLimit = 8,
-  kMaxDurationLessThanDesiredDuration = 9,
-  kLimitNotPositive = 10,
-  kGoalPositionMismatch = 11
+  kDesiredDurationTooShort = 1,
+  kMaxDurationExceeded = 2,
+  kVelocityExceedsLimit = 3,
+  kAccelExceedsLimit = 4,
+  kMaxDurationLessThanDesiredDuration = 5,
+  kLimitNotPositive = 6,
+  kGoalPositionMismatch = 7
 };
 
 const std::unordered_map<uint, std::string> kErrorCodeMap(
   {{kNoError, "No error, trajectory generation was successful"},
-   {kQuaternionLengthMismatch, "Internal error: Length mismatch between quaternion components"},
-   {kTranslationRotationLengthMismatch, "Internal error: Length mismatch between translation and rotation"},
    {kDesiredDurationTooShort, "Desired duration is too short, cannot have less than one timestep in a "
     "trajectory"},
-   {kCurrentOrientationNotNormalized, "Current orientation input quaternion is not normalized"},
-   {kGoalOrientationNotNormalized, "Goal orientation quaternion is not normalized"},
    {kMaxDurationExceeded, "Max duration was exceeded"},
    {kVelocityExceedsLimit, "A velocity input exceeds the velocity limit"},
    {kAccelExceedsLimit, "An acceleration input exceeds the acceleration limit"},
