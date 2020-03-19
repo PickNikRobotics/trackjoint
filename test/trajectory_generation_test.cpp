152a895c7c2d5485d1a10fcbab9dabb258c5e0ae
AndyZe <andyz@utexas.edu>
2020-03-18 21:03:32 -0500

    Fix silly but serious mistake in ForwardLimitCompensation

--- test/trajectory_generation_test.cpp
+++ test/trajectory_generation_test.cpp
@@ -675,7 +675,7 @@ TEST_F(TrajectoryGenerationTest, DurationExtension)
   const double kPositionError = trackjoint::CalculatePositionAccuracy(goal_joint_states, output_trajectories);
   EXPECT_LT(kPositionError, kPositionTolerance);
   // Duration
-  const double kExpectedDuration = 3.984;
+  const double kExpectedDuration = 3.87;
   const double kDurationTolerance = 5e-3;
   size_t vector_length = output_trajectories[0].elapsed_times.size() - 1;
   EXPECT_NEAR(output_trajectories[0].elapsed_times(vector_length), kExpectedDuration, kDurationTolerance);
