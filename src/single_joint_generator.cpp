93a75668bbf7e2f3da37d09a120f9a75921ad449
AndyZe <andyz@utexas.edu>
2020-05-06 15:00:57 -0500

    Set up if-condition to skip splines, if necessary

--- src/single_joint_generator.cpp
+++ src/single_joint_generator.cpp
@@ -71,20 +71,38 @@ ErrorCodeEnum SingleJointGenerator::generateTrajectory()
 
 void SingleJointGenerator::extendTrajectoryDuration()
 {
-  // Fit and generate a spline function to the original (time, position)
-  Eigen::RowVectorXd time(waypoints_.elapsed_times);
-  Eigen::RowVectorXd position(waypoints_.positions);
+  size_t new_num_waypoints = 1 + desired_duration_ / timestep_;
 
-  const auto fit = SplineFitting1D::Interpolate(position, 2, time);
-  Spline1D spline(fit);
+  // If waypoints were successfully generated for this dimension previously, just stretch the trajectory with splines.
+  // This is the best way because it reduces overshoot.
+  // Otherwise, re-generate a new trajectory from scratch.
+  if (false /*index_last_successful_ == static_cast<size_t>(waypoints_.positions.size() - 1)*/)
+  {
+    // Fit and generate a spline function to the original (time, position)
+    Eigen::RowVectorXd time(waypoints_.elapsed_times);
+    Eigen::RowVectorXd position(waypoints_.positions);
 
-  // New times, with the extended duration
-  size_t expected_num_waypoints = 1 + desired_duration_ / timestep_;
-  waypoints_.elapsed_times.setLinSpaced(expected_num_waypoints, 0., desired_duration_);
-  // Retrieve new positions at the new times
-  waypoints_.positions.resize(expected_num_waypoints);
-  for (size_t idx = 0; idx < waypoints_.elapsed_times.size(); ++idx)
-    waypoints_.positions[idx] = spline(waypoints_.elapsed_times(idx)).coeff(0);
+    const auto fit = SplineFitting1D::Interpolate(position, 2, time);
+    Spline1D spline(fit);
+
+    // New times, with the extended duration
+    waypoints_.elapsed_times.setLinSpaced(new_num_waypoints, 0., desired_duration_);
+    // Retrieve new positions at the new times
+    waypoints_.positions.resize(new_num_waypoints);
+
+    for (size_t idx = 0; idx < waypoints_.elapsed_times.size(); ++idx)
+      waypoints_.positions[idx] = spline(waypoints_.elapsed_times(idx)).coeff(0);
+
+    return;
+  }
+
+  // Clear previous results
+  waypoints_ = JointTrajectory();
+  waypoints_.elapsed_times.setLinSpaced(new_num_waypoints, 0., desired_duration_);
+  waypoints_.positions = interpolate(waypoints_.elapsed_times);
+  calculateDerivativesFromPosition();
+  ErrorCodeEnum error_code = forwardLimitCompensation(&index_last_successful_);
+  return;
 }
 
 JointTrajectory SingleJointGenerator::getTrajectory()
