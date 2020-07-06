440ee3f855597ebecb986969fa42a4ffde445c6f
AndyZe <andyz@utexas.edu>
2020-07-06 08:13:10 -0500

    Fix spline interpolation

--- src/single_joint_generator.cpp
+++ src/single_joint_generator.cpp
@@ -86,11 +86,18 @@ void SingleJointGenerator::extendTrajectoryDuration()
   // Otherwise, re-generate a new trajectory from scratch.
   if (index_last_successful_ == static_cast<size_t>(waypoints_.elapsed_times.size() - 1))
   {
-    // Fit and generate a spline function to the original (time, position)
-    Eigen::RowVectorXd time(waypoints_.elapsed_times);
+    std::cout << "Stretching trajectory!" << std::endl;
+    std::cout << "New desired duration: " << desired_duration_ << std::endl;
+
+    // Fit and generate a spline function to the original positions, same number of waypoints, new (extended) duration
+    // This only decreases velocity/accel/jerk, so no worries re. limit violation
+    //Eigen::VectorXd new_times;
+    //new_times.setLinSpaced(waypoints_.elapsed_times.size(), 0, desired_duration_);
+    Eigen::RowVectorXd new_times;
+    new_times.setLinSpaced(waypoints_.elapsed_times.size(), 0, desired_duration_);
     Eigen::RowVectorXd position(waypoints_.positions);
 
-    const auto fit = SplineFitting1D::Interpolate(position, 2, time);
+    const auto fit = SplineFitting1D::Interpolate(position, 2, new_times);
     Spline1D spline(fit);
 
     // New times, with the extended duration
@@ -101,17 +108,23 @@ void SingleJointGenerator::extendTrajectoryDuration()
     for (size_t idx = 0; idx < waypoints_.elapsed_times.size(); ++idx)
       waypoints_.positions[idx] = spline(waypoints_.elapsed_times(idx)).coeff(0);
 
+    std::cout << "Final waypoint after stretching: " << waypoints_.positions[waypoints_.positions.size() - 1] << std::endl;
+
     calculateDerivativesFromPosition();
     return;
   }
 
   // Plan a new trajectory from scratch:
   // Clear previous results
-  waypoints_ = JointTrajectory();
-  waypoints_.elapsed_times.setLinSpaced(new_num_waypoints, 0., desired_duration_);
-  waypoints_.positions = interpolate(waypoints_.elapsed_times);
-  calculateDerivativesFromPosition();
-  ErrorCodeEnum error_code = forwardLimitCompensation(&index_last_successful_);
+  else
+  {
+    waypoints_ = JointTrajectory();
+    waypoints_.elapsed_times.setLinSpaced(new_num_waypoints, 0., desired_duration_);
+    waypoints_.positions = interpolate(waypoints_.elapsed_times);
+    calculateDerivativesFromPosition();
+    ErrorCodeEnum error_code = forwardLimitCompensation(&index_last_successful_);
+  }
+
   return;
 }
 
